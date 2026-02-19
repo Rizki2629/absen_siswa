7dafcb68bc9c50ac780a42922f023f98112b6a4c
RizkiDev26 <rizkigu09@gmail.com>
2026-02-19 23:54:13 +0700

    temp: find student script

---
+++ find_student.php
@@ -0,0 +1,16 @@
+<?php
+require 'vendor/autoload.php';
+$url = parse_url(getenv('JAWSDB_URL'));
+$db = new mysqli(
+    $url['host'],
+    $url['user'],
+    $url['pass'],
+    ltrim($url['path'], '/'),
+    $url['port'] ?? 3306
+);
+if ($db->connect_error) { die("Connect error: " . $db->connect_error); }
+$res = $db->query("SELECT id, nama, nisn, parent_phone FROM students WHERE nisn='3148663143' OR nama LIKE '%Mirza%'");
+while ($row = $res->fetch_assoc()) {
+    echo "ID: {$row['id']} | Nama: {$row['nama']} | NISN: {$row['nisn']} | Phone: {$row['parent_phone']}\n";
+}
+$db->close();
